59769b2ed438e63843e704b60d5660c56862e786
Dimitri Géricot <dimitri.gericot@gmail.com>
2021-05-25 18:54:05 +0200

    Amélioration du header et intégration du carousel et des cartes en js

---
+++ frontend/js/fetch.js
@@ -0,0 +1,20 @@
+const getCameras = async function () {
+    try {
+        let response = await fetch('http://localhost:3000/api/cameras')
+        if (response.ok) {
+            let data = await response.json()
+            console.log(data)
+        } else {
+            console.error('Retour du serveur: ', response.status)
+        }
+    } catch (e) {
+        console.log(e)
+    }   
+   }
+
+   getCameras()
+
+
+
+
+    
