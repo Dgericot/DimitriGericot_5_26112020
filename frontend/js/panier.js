b75d02781df707f9da55c5684088f39ec5455e70
Dimitri Géricot <dimitri.gericot@gmail.com>
2021-06-16 21:43:38 +0200

    Ajout bouton suppression ligne produit, nouvelle colonne tableau lentilles, fonctions pour envoi des infos au serveur afin de récupérer ID de commande

--- frontend/js/panier.js
+++ frontend/js/panier.js
@@ -1,4 +1,5 @@
 import {toPrice} from './helpers'
+import {sendOrder} from './fetch'
 
 //Récupération des produits via le localStorage
 const storedCart = JSON.parse(localStorage.getItem("cart"));
@@ -6,7 +7,33 @@ const storedCart = JSON.parse(localStorage.getItem("cart"));
 
 displayOrder(storedCart);
 displayForm();
+sendOrder(createJsonBody, getInfosFromClient);
 
+//Retirer un article du panier
+document.getElementById("suppress-line").addEventListener('click',() => {
+    if( confirm( "Êtes-vous sûr de vouloir retirer cet article de votre panier ?"  )) {
+        localStorage.clear("selectedProduct");
+        window.location.href = "panier.html";
+         }
+      } )
+
+//Vider le panier totalement
+document.getElementById("empty-cart").addEventListener('click', () => {
+    if( confirm( "Êtes-vous sûr de vouloir vider votre panier ? \n\nVous perdrez toute votre sélection actuelle" )) {
+        localStorage.clear();
+        window.location.href = "index.html";
+           
+     }  
+/*
+//Envoi l'order et récupère l'Id de commande
+document.getElementById("order-button").addEventListener('click', (event) => { 
+    if (document.getElementById("contact-form")) {
+        event.preventDefault();
+        sendOrder(createJsonBody(), getInfosFromClient());
+        window.location.href = "order-validation.html";
+    }*/
+
+});
 
 
 function displayOrder (cart) {
@@ -15,7 +42,9 @@ function displayOrder (cart) {
     const productsRows = cart.products.map(product => {
         return `
         <tr>
+            <td><a role="button" id="suppress-line" class="btn btn-danger text-white"><i class="fas fa-trash-alt"></i></a></td>
             <td>${product.name}</td>
+            <td>${product.lenses}</td>
             <td>${toPrice(product.price)}</td>
             <td>1</td>
             <td>${toPrice(product.price)}</td>
@@ -24,13 +53,15 @@ function displayOrder (cart) {
         `
     })
     const render = `
-    <div class="col">
-    <h2 class="mb-2 mt-4 font-weight-normal text-lg-right"><i class="fas fa-shopping-basket mr-3"></i> Finaliser ma commande</h2>
+    <div class="col-12 ">
+    <h2 class="mb-2 mt-4 ml-2 font-italic text-left text-info"><i class="fas fa-shopping-basket mr-3"></i> Finaliser ma commande</h2>
     <table class="table text-center align-middle mt-5">
     
     <thead class="bg-light text-primary">
         <tr>
-            <th class="text-md-right">Produit</th>
+            <th>Retirer article</th>
+            <th class="text-md-center">Produit</th>
+            <th>Lentille</th>
             <th class="d-none d-md-table-cell">Prix unitaire</th>
             <th>Quantité</th>
             <th>Prix total</th>
@@ -41,9 +72,9 @@ function displayOrder (cart) {
     </tbody>
     </table>
     </div>
-    
+    <div class="row">
     <div class="col-12 col-sm-6">
-    <a role="button" id="empty-cart" class="btn btn-danger text-white ml-1 ml-lg-5 my-3 font-weight-bold"><i class="fas fa-trash-alt"></i> Vider mon panier</a>
+    <a role="button" id="empty-cart" class="btn btn-danger text-white ml-1 ml-lg-5 my-5 font-weight-bold"><i class="fas fa-trash-alt"></i> Vider mon panier</a>
     </div>
     <div class="col-12 col-sm-6">
     <p class="text-right ml-auto bg-light">
@@ -59,8 +90,10 @@ function displayOrder (cart) {
     <span class="font-weight-bold text-primary mr-3 mr-lg-5">INCLUSE DANS LE TARIF</span>
     </p>
     </div>
+    </div>
+    </div>
         `
-        main.innerHTML =`<div class="row">${render}</div>`; 
+        main.innerHTML =`${render}`; 
 
         }
 
@@ -70,24 +103,24 @@ function displayForm() {
 
     const affichage = `
 
-    <div class="col">
+    
     <form class="row" id="contact-form">
-    <div class="col-12 col-md-6 text-primary font-weight-bold was-validated">
-    <h3 class="my-3 my-md-5 h6">Veuillez renseigner les informations suivantes</h3>
+    <div class="col-12 col-md-6 text-primary ml-auto font-weight-bold was-validated">
+    <h3 class="my-3 my-md-5 h6">Veuillez renseigner les informations suivantes pour l'envoi et la facturation de votre commande</h3>
     
     <div class="form-group">
     <label for="last-name">Nom :</label>
-    <input type="text" pattern="#[^0-9]#[A-Z][A-Za-z\é\è\ê\-\']" class="form-control border-secondary" id="last-name" placeholder="Nom de famille" required>  
+    <input type="text" pattern="#[^0-9]#[A-Z][A-Za-z\é\è\ê\-\']" class="form-control border-secondary" id="lastName" placeholder="Nom de famille" required>  
     </div>
     
     <div class="form-group">
     <label for="first-name">Prénom :</label>
-    <input type="text" pattern="#[^0-9]#[A-Z][A-Za-z\é\è\ê\-\']" class="form-control border-secondary" id="first-name" placeholder="Prénom(s)" required>
+    <input type="text" pattern="#[^0-9]#[A-Z][A-Za-z\é\è\ê\-\']" class="form-control border-secondary" id="firstName" placeholder="Prénom(s)" required>
     </div>
     
     <div class="form-group">
-    <label for="mail">Email :</label>
-    <input id="mail" type="email" pattern="[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$)/;" class="form-control border-secondary"  placeholder="exemple@mail.com" required>
+    <label for="email">Email :</label>
+    <input id="email" type="email" pattern="[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$)/;" class="form-control border-secondary"  placeholder="exemple@mail.com" required>
     </div>
     
     <div class="form-group">
@@ -121,52 +154,46 @@ function displayForm() {
                 
     </div>
     <p id="error" class="h5 text-danger"></p>
-    <input type="submit" id="order-button" class="btn btn-success text-white font-weight-bold ml-3 my-5 px-3" value="Valider la commande">
+    <input type="submit" id="order-button" class="btn btn-success text-white font-weight-bold ml-5 my-5 px-3" value="Valider la commande">
     </form> 
-    
-    </div>
-    
     `;
 
-    section.innerHTML =`${affichage}`; 
+    section.innerHTML =`<div class="col-12">${affichage}</div>`; 
 
 }
 
-/*
-let contact = {
-        firstName: prenom,
-        lastName: nom,
-        address: adresse,
-        email: email,
-        city: ville,
+let productArray = JSON.parse(localStorage.getItem("products"));
+
+function createIdArray(productObject) {
+    let product_id = productObject.map((product) => product.prodId);
+    return product_id;
+}
+
+function createJsonBody() {
+    let jsonBody = {
+        contact: getInfosFromClient(),
+        products: createIdArray(productArray),
     };
+    return jsonBody;
+}
 
+function getInfosFromClient() {
+    let clientInfos = {
+        firstName: document.getElementById("firstName"),
+        lastName: document.getElementById("lastName"),
+        address: document.getElementById("address"),
+        city: document.getElementById("city"),
+        email: document.getElementById("email"),
+    };
+    return clientInfos;
+}
 
-const postOrderId = async function (order) {
-    
-        try {
-            let response = await fetch('http://localhost:3000/api/cameras/order)
-            if (response.ok) {
-                return await response.json()
-            } else {
-                console.error('Retour du serveur: ', response.status)
-            }
-        } catch (e) {
-            console.log(e)
-        }   
-       }   
-
-
-
-
-
-/****************************************************/
-/*Elément parent de panier.html
-const $order = document.querySelector('#order');
-
-document.getElementById("empty-cart").addEventListener('click', function() {
-    if( confirm( "Êtes-vous sûr de vouloir vider votre panier ? Vous perdrez toute votre sélection actuelle" )) {
-        localStorage.clear();
-        location.reload();
+function cartEmpty() {
+    if (localStorage.getItem("products") === null) {
+        return true;
+    } else {
+        const cart = JSON.parse(localStorage.getItem("products"));
+        return cart == 0;
     }
-})*/
+}
+
