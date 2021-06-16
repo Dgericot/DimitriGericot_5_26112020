98eb0a7f86ef61fc1a13c213a9a8f04ec0df9ca8
Dimitri Géricot <dimitri.gericot@gmail.com>
2021-06-16 21:38:10 +0200

    Ajout fonction sendOrder

--- frontend/js/fetch.js
+++ frontend/js/fetch.js
@@ -28,6 +28,26 @@ export const getCameras = async function () {
     }   
    }   
 
-  
+    export function sendOrder(jsonObject, clientInfos) {
+    if (cartEmpty()) {
+        window.alert("Votre panier est vide");
+    } else {
+        fetch("http://localhost:3000/api/cameras/order", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(jsonObject),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error("Problème survenu lors de l'envoi des données");
+            })
+        }
+
+    }
 
    
