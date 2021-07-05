33d276fca05ce1977c724cbb74eabeb4c1913595
Dimitri Géricot <dimitri.gericot@gmail.com>
2021-07-05 21:27:14 +0200

    Ajout const sendOrder

--- frontend/js/fetch.js
+++ frontend/js/fetch.js
@@ -12,9 +12,6 @@ export const getCameras = async function() {
 }
 
 
-
-
-
 export const getCameraById = async function(id) {
     try {
         let response = await fetch('http://localhost:3000/api/cameras/' + id)
@@ -26,4 +23,21 @@ export const getCameraById = async function(id) {
     } catch (e) {
         console.log(e)
     }
+}
+
+export const sendOrder = async function(order) {
+
+    fetch("http://localhost:3000/api/cameras/order", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(order)
+        }).then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error("Problème survenu lors de l'envoi des données");
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
 }
