e9b43bfd5fce14de0f5011b1dfb917b9e22b10a9
Dimitri Géricot <dimitri.gericot@gmail.com>
2021-07-13 09:13:40 +0200

    Ajout commentaires et indentation

--- frontend/js/order-validation.js
+++ frontend/js/order-validation.js
@@ -2,6 +2,7 @@ import { toPrice } from './helpers'
 
 displayOrderId();
 
+//Récupère l'order Id et le prix total du panier du localStorage et les affiches sur la page. Applique ensuite un nettoyage du localStorage 
 function displayOrderId() {
     const main = document.querySelector('#order-valide .container');
     let data = localStorage.getItem("orderId");
@@ -12,7 +13,7 @@ function displayOrderId() {
             <p class="text-center">Votre commande à été validée et vous recevrez un email de confirmation dans quelques minutes</p>
             <p class="text-center id-confirmation">Votre numéro de commande est le suivant:${data}</p>
             <p class="text-center id-confirmation">Le montant total de votre commande est de ${toPrice(totalOrderAmount)}</p> 
-            <img src="/completed.svg" class="w-50 mt-4" alt="Votre commande est validée">
+            <img src="/completed.svg" class="w-50 mt-4 ml-4" alt="Votre commande est validée">
     `
     main.innerHTML = `<div class = "col-12 text-center">${pageConfirmation}</div>`
     localStorage.clear();
